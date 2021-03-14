d896dedd7a0fa6a74aff21a2de3a8255ec3b666f
galluccichiara <73693985+galluccichiara@users.noreply.github.com>
2021-03-14 21:34:32 +0100

    Update R_code_EXAM.r

--- R_code_EXAM.r
+++ R_code_EXAM.r
@@ -12,50 +12,46 @@ setwd("C:/lab/") #setwd function= setting a new working directory
 albedo <- raster("c_gls_ALBH_202005030000_GLOBE_PROBAV_V1.5.1.nc")
 plot(albedo) #plot = plotting/showing of R objects
 cl <- colorRampPalette (c('green','orange','yellow')) (100) #colorRampPalette = using and edit color schemes, yellow is used for maximum values because it is the colour that has the maximum impact to our eyes, 100 is the number of color in the used color scale; #c= setting things ("c" is for "characters") before the array
-plot(albedo, col=cl, main ="Albedo 15/04-15/05/20") #main = giving a title
+plot(albedo, col=cl, main ="ALBEDO 15/04-15/05/20") #main = giving a title
 
 #let's do the same regarding Vegetation Properties - FAPAR 300m V1 during the period: 13/12/2019-31/08/2020
 fapar <- raster("c_gls_FAPAR300_202007100000_GLOBE_PROBAV_V1.0.1.nc")
-plot(fapar, main="FAPAR 13/12/2019-31/08/2020")
 cl <- colorRampPalette (c('black','yellow','green')) (100)
 plot(fapar, col=cl, main ="FAPAR 13/12/2019-31/08/2020")
 
 #let's do the same regarding Vegetation Properties - LAI 300m V1 during the period: 13/12/2019-31/08/2020
 lai <- raster("c_gls_LAI300_202007100000_GLOBE_PROBAV_V1.0.1.nc")
-plot(lai)
-plot(lai, main="LAI 13/12/2019-31/08/2020")
 cl <- colorRampPalette (c('black','yellow','green')) (100)
 plot(lai, col=cl, main ="LAI 13/12/2019-31/08/2020")
 
 #focus on a specific area in order to analyse the correlation
 ext <- c(0,50,40,60) #ext = defining minimum and maximum of x, y variables
 EUALBEDO <- crop (albedo, ext) #crop= zooming in on a specific part of the map (the specific area analyzed), it's for geographic subset; #,ext = the extension previously declared
-cl <- colorRampPalette (c('green','orange','yellow')) (100)
-plot(EUALBEDO, col=cl, main ="EUALBEDO 15/04-15/05/20")
+cl <- colorRampPalette (c('green','blue','yellow')) (100)
+plot(EUALBEDO, col=cl, main ="EU.ALBEDO 15/04-15/05/20")
 
 #let's do the same with FAPAR
 ext <- c(0,50,40,60) 
 EUFAPAR <- crop (fapar, ext)
-cl <- colorRampPalette (c('black','yellow','green')) (100)
-plot(EUFAPAR, col= cl, main="EUFAPAR 13/12/2019-31/08/2020")
+cl <- colorRampPalette (c('brown','yellow','red')) (100)
+plot(EUFAPAR, col= cl, main="EU.FAPAR 13/12/2019-31/08/2020")
 
 #let's do the same with LAI
 ext <- c(0,50,40,60)
 EULAI <- crop (lai, ext)
 cl <- colorRampPalette (c('black','yellow','green')) (100)
-plot(EULAI, col= cl, main="EULAI 13/12/2019-31/08/2020")
+plot(EULAI, col= cl, main="EU.LAI 13/12/2019-31/08/2020")
 
 
-#???????????let's plot all the 3 maps together in a matrix
 par(mfrow=c(1,3)) #par = setting graphical parameters => par(mfrow = c (nrows,ncolumns) = creating a matrix of nrows, ncolumns to plot the two obtained maps together and compare them
-plot(albedo, col=cl)
-plot(fapar, col=cl) 
-plot(lai,col=cl) 
+plot(EUFAPAR, col=cl,main="EU.FAPAR 13/12/2019-31/08/2020") 
+plot(EUALBEDO, col=cl,main="EU.ALBEDO 15/04-15/05/20")
+plot(EULAI,col=cl, main="EU.LAI 13/12/2019-31/08/2020")
 
-# Otherwise let's produce a stack(with a list of data we can put all together in one image)
-#the otuput is the same,but in this case we plot more easily all images with just one line)
-AFL <- stack(albedo,fapar,lai)
-plot(AFL, col=cl)
+
+
+dif <- EUFAPAR-EULAI
+plot(dif)
 
 
 #the outcomes show the huge amount of NO2 in January decreased during the following two months due to the main causes already described in the introduction: COVID-19, level of pollution, market crisis
