b3df1dbd5987d1c0c7b130dc7f7779b2eae8c4d3
galluccichiara <73693985+galluccichiara@users.noreply.github.com>
2021-03-16 11:08:45 +0100

    Update R_code_EXAM.r

--- R_code_EXAM.r
+++ R_code_EXAM.r
@@ -7,30 +7,30 @@ library(ncdf4)
 library(raster) #raster package (already installed with the function install.packages("raster")) = using spatial data; 
 setwd("C:/lab/") #setwd function= setting a new working directory
 
-#let's import image about Hemispherical Albedo 1km Global V1 during the period: 15/04/2020-15/05/2020
+#let's import image about Hemispherical Albedo 1km Global V1 during the period: 15/04/2020-15/05/2020 -> this specific timeframe as "reference mean period"
 #raster function = import and read data; #albedo <- = giving a name to this data
-albedo <- raster("c_gls_ALBH_202005030000_GLOBE_PROBAV_V1.5.1.nc")
-plot(albedo) #plot = plotting/showing of R objects
+albedo <- raster("albedo1.nc")
 cl <- colorRampPalette (c('green','orange','yellow')) (100) #colorRampPalette = using and edit color schemes, yellow is used for maximum values because it is the colour that has the maximum impact to our eyes, 100 is the number of color in the used color scale; #c= setting things ("c" is for "characters") before the array
+#plot = plotting/showing of R objects
 plot(albedo, col=cl, main ="ALBEDO 15/04-15/05/20") #main = giving a title
 
 #let's do the same regarding Vegetation Properties - FAPAR 300m V1 during the period: 13/12/2019-31/08/2020
-fapar <- raster("c_gls_FAPAR300_202007100000_GLOBE_PROBAV_V1.0.1.nc")
+fapar <- raster("fapar.nc")
 cl <- colorRampPalette (c('black','yellow','green')) (100)
 plot(fapar, col=cl, main ="FAPAR 13/12/2019-31/08/2020")
 
 #let's do the same regarding Vegetation Properties - LAI 300m V1 during the period: 13/12/2019-31/08/2020
-lai <- raster("c_gls_LAI300_202007100000_GLOBE_PROBAV_V1.0.1.nc")
+lai <- raster("lai.nc")
 cl <- colorRampPalette (c('black','yellow','green')) (100)
 plot(lai, col=cl, main ="LAI 13/12/2019-31/08/2020")
 
-#focus on a specific area in order to analyse the correlation
+#focus on a specific area in order to analyse the correlation-> this area, whose extent may be overlapped to Europe extent, is representative in order to understand my case study
 ext <- c(0,50,40,60) #ext = defining minimum and maximum of x, y variables
 EUALBEDO <- crop (albedo, ext) #crop= zooming in on a specific part of the map (the specific area analyzed), it's for geographic subset; #,ext = the extension previously declared
-cl <- colorRampPalette (c('green','blue','yellow')) (100)
+cl <- colorRampPalette (c('green','blue','yellow')) (100) 
 plot(EUALBEDO, col=cl, main ="EU.ALBEDO 15/04-15/05/20")
 
-#let's do the same with FAPAR
+#let's do the same with FAPAR in order to obtain plot having the same extent
 ext <- c(0,50,40,60) 
 EUFAPAR <- crop (fapar, ext)
 cl <- colorRampPalette (c('brown','yellow','red')) (100)
@@ -42,14 +42,18 @@ EULAI <- crop (lai, ext)
 cl <- colorRampPalette (c('black','yellow','green')) (100)
 plot(EULAI, col= cl, main="EU.LAI 13/12/2019-31/08/2020")
 
-
+#let's graphically compare the three plots and let's locate the plot regarding EU.ALBEDO in-between EU.FAPAR and EU.LAI respectively in order to have a clearer overall picture about albedo-fapar and albedo-lai correlationship
 par(mfrow=c(1,3)) #par = setting graphical parameters => par(mfrow = c (nrows,ncolumns) = creating a matrix of nrows, ncolumns to plot the two obtained maps together and compare them
 plot(EUFAPAR, col=cl,main="EU.FAPAR 13/12/2019-31/08/2020") 
 plot(EUALBEDO, col=cl,main="EU.ALBEDO 15/04-15/05/20")
 plot(EULAI,col=cl, main="EU.LAI 13/12/2019-31/08/2020")
 
+# With regards to Albedo values, since I chose the products observed during the period bounded btw 15/04 and 15/05/20 as reference example, I would like to 
 
-
+# carica lista di Albedo da 13/12 a 31/08 
+# stack
+# dif tra primo e ultimo mese
+# multivariate analysis 
 dif <- EUFAPAR-EULAI
 plot(dif)
 
@@ -61,5 +65,5 @@ cldif <- colorRampPalette (c('blue','black','yellow')) (100) #where the new map 
 plot(difNO2, col=cldif)
 
 
-#???? multivariate plot 
+#cosa notiamo + aggiusta colori 
 #???? funzione salvataggio
